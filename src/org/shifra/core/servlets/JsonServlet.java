fccc88dbef7e77ceb2b809bbb37fe8739cec2d0c
Shifra Pride Raffel <spridera@Shifra-Pride-Raffels-MacBook-Pro.local>
2015-06-05 14:57:40 -0700

    be able to parse a list of simple objects from a file

--- src/org/shifra/core/servlets/JsonServlet.java
+++ src/org/shifra/core/servlets/JsonServlet.java
@@ -6,8 +6,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author spridera
@@ -21,13 +24,20 @@ public class JsonServlet extends HttpServlet {
                 //this is actually useful to have turned on, otherwise we get null
 //                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
                 .configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true).
-                //may not want this either
-                configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+                        //may not want this either
+                                configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
         Glossary g = mapper.readValue("{\n" +
 //                "    \"glossary\": {\n" +
                 "        \"title\": \"example glossary\"\n" +
 //                "    }\n" +
                 "}", Glossary.class);
+        String path = getServletContext().getRealPath("/WEB-INF/resource");
+        List<Glossary> fromFile = Arrays.asList(
+                mapper.readValue(new File(path + File.separator + "objectList.json"), Glossary[].class));
+        StringBuilder builder = new StringBuilder();
+        for (Glossary glossary : fromFile) {
+            builder.append("glossary from file: " + glossary.getTitle() + "<br />");
+        }
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         String docType =
@@ -38,7 +48,9 @@ public class JsonServlet extends HttpServlet {
                 "<HEAD><TITLE>Hello</TITLE></HEAD>\n" +
                 "<BODY BGCOLOR=\"#FDF5E6\">\n" +
                 "<H1>" + g.getTitle() +
-                "</H1>\n" +
+                "</H1>\n"
+                + builder.toString() +
+
                 "</BODY></HTML>");
     }
 }
